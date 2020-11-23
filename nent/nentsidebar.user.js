36c32f0ebe704f15348a4bd70a9b1a984108db52
Ruben <ruben.leikarnes@gmail.com>
2020-11-23 09:15:18 +0100

    Update nentsidebar.user.js

--- nent/nentsidebar.user.js
+++ nent/nentsidebar.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name        Production sidebar - nentsportbooking.com
 // @namespace   Violentmonkey Scripts
-// @match       https://www.nentsportbooking.com/productions/overview
+// @match       https://www.nentsportbooking.com/*
 // @grant       none
-// @version     1.0
+// @version     1.0.2
 // @author      -
 // @description 11/20/2020, 10:40:41 AM
 // ==/UserScript==
